fdd206f4a91d1c20a4d00ca1daff18bbe7a83728
lamiaa <lamiaaelewa38@gmail.com>
2025-02-14 08:58:02 -0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Moon-Website/', // استبدلي باسم الريبو بتاعك بالظبط
 })
+
